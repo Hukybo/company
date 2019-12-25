042f211a9f1c5bb1a8f2291f5c4730f1ab67f47c
hukybo <hukybo@gmial.com>
2019-12-25 17:50:40 +0800

    2019-12-25更新

--- index.php
+++ index.php
@@ -17,7 +17,15 @@
     // 产生一个凭证，使用常量（定义之后不能修改）
     define('TICKET', 1);
 
-    $filename = $m.'/'.$c.'.php';
+    require_once('smarty/Smarty.class.php');
+    $smarty = new Smarty;
+
+    // 更改smarty部分配置
+    $smarty->left_delimiter = "<{";
+    $smarty->right_delimiter = "}>";
+    $smarty->template_dir = array('./templates/'.$m.'/');
+
+    $filename = './templates/'.$m.'/'.$c.'.php';
     if (file_exists($filename)) {
         include_once($filename);
     } else {
